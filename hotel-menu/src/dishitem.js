dbb9e1532dccdf1a9cb3d7dbb434a786268121cb
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-03-28 22:29:59 +0530

    dishitem.js

---
+++ hotel-menu/src/dishitem.js
@@ -0,0 +1,53 @@
+
+const dishitem = [
+    {
+        id:1,
+
+        name: "Dosa",
+        img:"",
+        price:"100",
+        rating:"4",
+    },
+    {
+        id:2,
+
+        name: "Biriyani",
+        img:"",
+        price:"120",
+        rating:"4.5",
+    },
+    {
+        id:3,
+
+        name: "Pizza",
+        img:"",
+        price:"200",
+        rating:"4",
+    },
+    {
+        id:4,
+
+        name: "Momos",
+        img:"",
+        price: "120",
+        rating:"4.5",
+    },
+    {
+        id:5,
+
+        name:"Noodles",
+        img:"",
+        price:"100",
+        rating:"4",
+    },
+    {
+        id:5,
+
+        name: "Manchurian",
+        img:"",
+        price:"150",
+        rating:"5",
+    }
+]
+
+export default dishitem;
